7b4cde71021d2c682164894770db8c49e8e1b832
Clément Auger <clement.auger@epitech.eu>
2019-03-14 12:11:03 +0100

    test 2

--- calcul.py
+++ calcul.py
@@ -27,5 +27,24 @@ def combi_of_a_set(n, k):
 def bino_and_poisson(d):
     p_succes = d / (8 * 3600) #a day is 8h
     print(p_succes)
-
+    b_values = build_b_values(p_succes)
+    print_values(b_values, "Binomial")
+    p_values = build_p_values(p_succes)
+    print_values(p_values, "Poisson")
     return
+
+def build_b_values(p_succes):
+    return [[0, 0]]
+
+
+def build_p_values(p_succes):
+    return [[0, 0]]
+
+def print_values(values, method):
+    print(method, "distribution:", end='')
+    for pair in values:
+        print(pair[0], "->", "%.3f" % pair[1], end='')
+        if (pair[0] - 1 % 6 == 0):
+            print()
+        else:
+            print(end='\t')
